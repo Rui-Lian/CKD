4c2a430d13675222aca36b2771193adfb7fdbd9d
Lian Rui <rui_lian@hotmail.com>
2021-09-06 16:32:40 +0800

    Read excel all sheets

---
+++ excel_all_sheets.R
@@ -0,0 +1,10 @@
+# Read all excel sheets. 
+read_excel_allsheets <- function(filename, 
+                                 tibble = FALSE) {
+    sheets <- readxl::excel_sheets(filename)
+    x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+    if(!tibble) x <- lapply(x, as.data.frame)
+    names(x) <- sheets
+    x
+}
+## Source: https://stackoverflow.com/questions/12945687/read-all-worksheets-in-an-excel-workbook-into-an-r-list-with-data-frames
